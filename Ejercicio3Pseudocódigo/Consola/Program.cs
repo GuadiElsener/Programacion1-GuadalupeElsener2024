ccb68c96e152a2454e015cc9ffbd68f1f42445cf
GuadiElsener <elsener.guadalupe.ices@gmail.com>
2024-04-10 19:18:04 -0300

    Ejercicio 3 Commit
    Finalización del ejercicio 3

---
+++ Ejercicio3Pseudocódigo/Consola/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("ingrese el lado del cuadrado: ");
+Double lado1; 
+
+if (!Double.TryParse(Console.ReadLine(), out lado1))
+{
+    Console.WriteLine("Error, ingrese una medida correcta");
+}
+else
+{
+    Double resultado = lado1 * lado1;
+    Console.WriteLine("El área del cuadrado es de: " +  resultado);
+}
+   
+
